90873c5d35f5e036be9a774d72a8606620bec2d4
Paul Vangelakos <paulvangelakos@gmail.com>
2018-10-28 17:09:44 -0600

    Styling on cards

--- src/Card.js
+++ src/Card.js
@@ -17,14 +17,14 @@ export default class Card extends Component {
     console.log(this.props)
     return ( 
       <div className="card" >
-        <h1> Trail Name: {this.trailName} </h1> 
-        <h2> Park Name: {this.parkName} </h2> 
-        <div className="side-items">
+        <h1><span className="trail-name-title">Trail:</span> {this.trailName} </h1> 
+        <h2> <span className="park-name-title">National Park:</span> {this.parkName} </h2> 
+        <div className="distance-difficulty-container">
           <div className="difficulty-rating" >
-            <p>Difficulty Rating: {this.difficultyRating}</p>
+            <p><span className="difficulty-rating-text">Difficulty Rating: </span>{this.difficultyRating}</p>
           </div>
           <div className="distance-roundtrip"> 
-            <p>Distance Roundtrip:{this.distanceRoundtripMiles}</p>
+            <p><span className="distance-roundtrip-text">Distance Roundtrip: </span>{this.distanceRoundtripMiles}</p>
           </div>
         </div> 
       </div>
